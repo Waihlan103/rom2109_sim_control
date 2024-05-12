d146b2bbb5fd4a486ae81130c1fd18b12ab93300
Waihlan Kaung <waihlan103@gmail.com>
2024-05-12 12:36:30 +0630

    first commit

--- rom2109_gazebo/launch/rom2109_ros2_control.launch.py
+++ rom2109_gazebo/launch/rom2109_ros2_control.launch.py
@@ -56,8 +56,8 @@ def generate_launch_description():
     spawn_robot_node = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        # arguments=['-database', 'rom2109_tall_ros', '-entity', 'rom2109_tall_ros',
-        arguments=['-file', urdf_file, '-entity', 'rom2109_tall_ros',
+        arguments=['-database', 'rom2109_tall_ros', '-entity', 'rom2109_tall_ros',
+        # arguments=['-file', urdf_file, '-entity', 'rom2109_tall_ros',
                    "-x", '0.0',
                    "-y", '0.0',
                    "-z", '0.3'],
